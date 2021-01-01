26d5bd5b92abe2a78cdb8c48984beca861eddad5
Rob Dirnens <42729985+rrdirnens@users.noreply.github.com>
2021-01-01 12:51:44 +0200

    Added sass

---
+++ vue-code-example/src/components/HeaderComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="header">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HeaderComponent',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+
