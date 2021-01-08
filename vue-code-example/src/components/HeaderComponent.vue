a6b9444d930b3222c0b3bba4c4cf9b3c63d9e7cb
Rob Dirnens <42729985+rrdirnens@users.noreply.github.com>
2021-01-08 12:43:10 +0200

    Added footer, WIP sticky header

--- vue-code-example/src/components/HeaderComponent.vue
+++ vue-code-example/src/components/HeaderComponent.vue
@@ -13,22 +13,12 @@
       <a href="javascript:;" class="link header__link">Online Experiences</a>
     </div>
     <div class="header__right">
-      <a href="javascript:;" class="button header__button">Become a host</a>
-      <a href="javascript:;" class="button header__button">
-        <svg
-          class="button__elem"
-          viewBox="0 0 16 16"
-          xmlns="http://www.w3.org/2000/svg"
-          aria-hidden="true"
-          role="presentation"
-          focusable="false"
-          style="display: block; height: 16px; width: 16px; fill: currentcolor"
-        >
-          <path
-            d="m8.002.25a7.77 7.77 0 0 1 7.748 7.776 7.75 7.75 0 0 1 -7.521 7.72l-.246.004a7.75 7.75 0 0 1 -7.73-7.513l-.003-.245a7.75 7.75 0 0 1 7.752-7.742zm1.949 8.5h-3.903c.155 2.897 1.176 5.343 1.886 5.493l.068.007c.68-.002 1.72-2.365 1.932-5.23zm4.255 0h-2.752c-.091 1.96-.53 3.783-1.188 5.076a6.257 6.257 0 0 0 3.905-4.829zm-9.661 0h-2.75a6.257 6.257 0 0 0 3.934 5.075c-.615-1.208-1.036-2.875-1.162-4.686l-.022-.39zm1.188-6.576-.115.046a6.257 6.257 0 0 0 -3.823 5.03h2.75c.085-1.83.471-3.54 1.059-4.81zm2.262-.424c-.702.002-1.784 2.512-1.947 5.5h3.904c-.156-2.903-1.178-5.343-1.892-5.494l-.065-.007zm2.28.432.023.05c.643 1.288 1.069 3.084 1.157 5.018h2.748a6.275 6.275 0 0 0 -3.929-5.068z"
-          ></path>
-        </svg>
-      </a>
+      <a
+        href="javascript:;"
+        class="button header__button header__button--large-screens-only"
+        >Become a host</a
+      >
+
       <button class="header__account-hamburger">
         <div
           :class="{ 'hamburger-toggle--open': hamburger }"
@@ -39,7 +29,7 @@
           <div class="hamburger-toggle__line"></div>
         </div>
         <svg
-          class="header__account-user-icon"
+          class="header__account-user-icon header__account-user-icon--large-screen-only"
           viewBox="0 0 32 32"
           xmlns="http://www.w3.org/2000/svg"
           aria-hidden="true"
@@ -52,14 +42,51 @@
         </svg>
       </button>
       <div v-if="hamburger" class="header__user-panel">
-        <a href="javascript:;" class="header__user-panel-link">Log in</a>
-        <a href="javascript:;" class="header__user-panel-link">Sign up</a>
+        <a
+          href="javascript:;"
+          class="header__user-panel-link header__user-panel-link--small-screen-only"
+          >Places to stay</a
+        >
+        <a
+          href="javascript:;"
+          class="header__user-panel-link header__user-panel-link--small-screen-only"
+          >Experiences</a
+        >
+        <a
+          href="javascript:;"
+          class="header__user-panel-link header__user-panel-link--small-screen-only"
+          >Online-experiences</a
+        >
+        <a
+          href="javascript:;"
+          class="header__user-panel-link header__user-panel-link--large-screen-only"
+          >Log in</a
+        >
+        <a
+          href="javascript:;"
+          class="header__user-panel-link header__user-panel-link--large-screen-only"
+          >Sign up</a
+        >
         <div class="header__user-panel-divider"></div>
-        <a href="javascript:;" class="header__user-panel-link"
+        <a
+          href="javascript:;"
+          class="header__user-panel-link header__user-panel-link--large-screen-only"
           >Host your home</a
         >
-        <a href="javascript:;" class="header__user-panel-link"
+        <a
+          href="javascript:;"
+          class="header__user-panel-link header__user-panel-link--large-screen-only"
           >Host your experience</a
+        >
+        <a
+          href="javascript:;"
+          class="header__user-panel-link header__user-panel-link--small-screen-only"
+          >User dashboard</a
+        >
+        <a
+          href="javascript:;"
+          class="header__user-panel-link header__user-panel-link--small-screen-only"
+          >Become a host</a
         >
         <a href="javascript:;" class="header__user-panel-link">Help</a>
       </div>
@@ -77,7 +104,15 @@ export default {
   },
 
   methods: {
-
+    stickyHeader() {
+      let navbar = document.querySelector(".header--sticky");
+      let sticky = navbar.offsetTop;
+      if (window.pageYOffset >= 160) {
+        navbar.classList.add("header--sticky-on");
+      } else {
+        navbar.classList.remove("header--sticky-on");
+      }
+    },
   },
 
   created() {
@@ -86,20 +121,24 @@ export default {
       "click",
       function (event) {
         if (self.hamburger === true) {
-          if (event.target.matches('.header__account-hamburger') || !event.target.closest(".header__user-panel")) {
-            console.log(event)
-            console.log('closing hamburger')
-            self.hamburger = false
+          if (
+            event.target.matches(".header__account-hamburger") ||
+            !event.target.closest(".header__user-panel")
+          ) {
+            self.hamburger = false;
           }
-        } else if (self.hamburger == false){
-          if (event.target.matches('.header__account-hamburger')) {
-            console.log('opening hamburger')
-            self.hamburger = true
+        } else if (self.hamburger == false) {
+          if (event.target.matches(".header__account-hamburger")) {
+              self.hamburger = true;
           }
         }
       },
       false
     );
+
+    window.onscroll = function () {
+      self.stickyHeader();
+    };
   },
 };
 </script>
