d025dc1897e8eb861a8522ddf50927dcca69d3fa
Rob Dirnens <42729985+rrdirnens@users.noreply.github.com>
2021-01-14 13:38:32 +0200

    Added dynamic functionality, works with any number of questions without updating code

--- vue-code-example/src/components/QuizComponent.vue
+++ vue-code-example/src/components/QuizComponent.vue
@@ -1,29 +1,36 @@
 <template>
   <div class="quiz">
-    <div v-if="quizCurrent == 0" class="no-questions">
+    <div v-if="quizPanelCurrent == 0" class="no-questions">
       start quiz?
 
       <div @click="changePanel()" class="next-panel">Let's gooooo</div>
     </div>
 
-    <div v-else-if="quizCurrent == 1" class="first-one">
-      first question
+    <div v-for="(question, index) in quizData" :key="index" class="quiz-wrapper">
+      <div v-if="quizPanelCurrent == index + 1" class="first-one">
+        {{index + 1}}st question:
 
-      <div @click="changePanel()" class="next-panel">Let's gooooo</div>
+        <div class="question-text">{{question.question}}</div>
+        <div class="answers">
+          <div v-for="(answer, index) in question.answers" :key="index" @click="updateScore(answer.points)" class="answer">
+            {{answer.text}}
+            {{answer.points}}
+          </div>
+        </div>
+
+        <div @click="changePanel()" class="next-panel">Let's gooooo</div>
+      </div>
     </div>
 
-    <div v-else-if="quizCurrent == 2" class="first-one">
-      second question
+    <div v-if="finished" class="fourth-one">
+      Your result: {{quizScore}}
 
-      <div @click="changePanel()" class="next-panel">Let's gooooo</div>
-    </div>
-    <div v-else-if="quizCurrent == 3" class="first-one">
-      third question
+      What does your score mean? 
+      0-5 points is excellent
+      5-30 points is good 
+      30-45 points means you're r-worded
 
-      <div @click="changePanel()" class="next-panel">Let's gooooo</div>
-    </div>
-    <div v-else-if="quizCurrent == 4" class="first-one">
-      Your result
+      <div v-if="quizScore > 5" class="">your score is over five which means you are not r-worded</div>
 
       <div @click="changePanel('back')" class="next-panel">
         Back to browsing
@@ -79,7 +86,7 @@ export default {
           },
         },
         {
-          question: "Where is the question?",
+          question: "When is the question?",
           answers: {
             option1: {
               text: "I don't know",
@@ -96,26 +103,170 @@ export default {
             },
           },
         },
+        {
+          question: "When is the question?",
+          answers: {
+            option1: {
+              text: "I don't know",
+              points: 1,
+            },
+            option2: {
+              text: "I do know",
+              points: 2,
+            },
+            option3: {
+              text:
+                "I am offended by the question and would like to speak to the manager",
+              points: 3,
+            },
+          },
+        },
+        {
+          question: "When is the question?",
+          answers: {
+            option1: {
+              text: "I don't know",
+              points: 1,
+            },
+            option2: {
+              text: "I do know",
+              points: 2,
+            },
+            option3: {
+              text:
+                "I am offended by the question and would like to speak to the manager",
+              points: 3,
+            },
+          },
+        },
+        {
+          question: "When is the question?",
+          answers: {
+            option1: {
+              text: "I don't know",
+              points: 1,
+            },
+            option2: {
+              text: "I do know",
+              points: 2,
+            },
+            option3: {
+              text:
+                "I am offended by the question and would like to speak to the manager",
+              points: 3,
+            },
+          },
+        },
+        {
+          question: "When is the question?",
+          answers: {
+            option1: {
+              text: "I don't know",
+              points: 1,
+            },
+            option2: {
+              text: "I do know",
+              points: 2,
+            },
+            option3: {
+              text:
+                "I am offended by the question and would like to speak to the manager",
+              points: 3,
+            },
+          },
+        },
+        {
+          question: "When is the question?",
+          answers: {
+            option1: {
+              text: "I don't know",
+              points: 1,
+            },
+            option2: {
+              text: "I do know",
+              points: 2,
+            },
+            option3: {
+              text:
+                "I am offended by the question and would like to speak to the manager",
+              points: 3,
+            },
+          },
+        },
+        {
+          question: "When is the question?",
+          answers: {
+            option1: {
+              text: "I don't know",
+              points: 1,
+            },
+            option2: {
+              text: "I do know",
+              points: 2,
+            },
+            option3: {
+              text:
+                "I am offended by the question and would like to speak to the manager",
+              points: 3,
+            },
+          },
+        }
       ],
       quizScore: null,
-      quizCurrent: 0,
+      quizScoreCurrent: null,
+      quizPanelCurrent: 0,
+      finished: false
     };
   },
   methods: {
+
+    //Runs when user clicks "Next Question"
     changePanel(action) {
       let self = this;
-      if (self.quizCurrent == 4 && action == "back") {
-        console.log("closing quiz");
-      } else if (self.quizCurrent == 4 && action == "again") {
-        console.log("retake quiz");
-        self.quizCurrent = 0;
+      let last = self.quizData.lastIndexOf(self.quizData[self.quizData.length - 1]) + 2
+      let oneBeforeLast = last - 1
+    
+      //Check if the panel being closed is the last question
+      if (self.quizPanelCurrent == oneBeforeLast) {
+        self.finished = true
       } else {
-        console.log("next panel");
-        self.quizCurrent += 1;
+        self.finished = false
+      }
+
+      //Check if it's the closing quiz panel AND the user selects the "Back to browsing"
+      if (self.quizPanelCurrent == last && action == 'back') {
+        // console.log('backk')
+        self.quizScore = null
+        self.quizScoreCurrent = null
+
+      //Check if it's the closing quiz panel AND the user selects the "Retake the quiz"
+      } else if (self.quizPanelCurrent == last && action == 'again') {
+        // console.log('againn')
+        self.quizPanelCurrent = 0
+        self.quizScore = null
+        self.quizScoreCurrent = null
+
+      //Run this when the user submits and answer
+      } else {
+        self.quizPanelCurrent += 1
+        self.quizScore += self.quizScoreCurrent
+        self.quizScoreCurrent = null
       }
     },
+    updateScore(points) {
+
+      //Run every time an answer is selected, but not submitted
+      //Check if a score for the current question has been registered.
+      //If it's not registered, the just add the score
+      if (this.quizScoreCurrent == null) {
+        this.quizScoreCurrent += points
+
+      //If it is registered, clear the score and then add the score
+      } else {
+        this.quizScoreCurrent = null
+        this.quizScoreCurrent += points
+      }
+    }
   },
 };
 </script>
-
-
